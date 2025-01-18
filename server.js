f29e6ad24957485aa968c85f8acd09c83244ba5f
Nobsmoke123 <donaldakobundu@gmail.com>
2025-01-18 12:44:14 +0100

    Implement Google OAuth authentication with Passport.js and session management

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const helmet = require("helmet");
 
 const path = require("path");
 
+const passport = require("passport");
+
+const { Strategy } = require("passport-google-oauth20");
+
+const cookieSession = require("cookie-session");
+
 require("dotenv").config();
 
 const checkLoggedInMiddleware = require("./middleware");
@@ -17,17 +23,100 @@ const PORT = 4000;
 const CONFIG = {
   CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: CONFIG.CLIENT_ID,
+  clientSecret: CONFIG.CLIENT_SECRET,
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log(`Google profile`, profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+// Save the session to the cookie
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Read the session from the cookie
+passport.deserializeUser((id, done) => {
+  // Do a database lookup for the ID.
+  //   User.findById(id).then((user) => {
+  //     done(null, user);
+  //   });
+  done(null, id);
+});
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/auth/google", (req, res) => {});
+app.use(
+  cookieSession({
+    secure: true,
+    name: "node-security-session",
+    maxAge: 24 * 60 * 60 * 1000, // in milliseconds
+    keys: [CONFIG.COOKIE_KEY_1, CONFIG.COOKIE_KEY_2],
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+// register regenerate & save after the cookieSession middleware initialization
+app.use(function (request, response, next) {
+  if (request.session && !request.session.regenerate) {
+    request.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (request.session && !request.session.save) {
+    request.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
+});
 
-app.get("/auth/logout", (req, res) => {});
+app.use(passport.initialize());
+
+// Authenticates the session and validates the session
+app.use(passport.session());
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    // session: true,
+  })
+);
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
+});
+
+app.get("/auth/logout", (req, res) => {
+  // Exposed on the req object by passport
+  req.logout(
+    {
+      keepSessionInfo: false,
+    },
+    (err) => {}
+  );
+  return res.redirect("/");
+});
 
 app.get("/secret", checkLoggedInMiddleware, (req, res) => {
   return res.send("Your personal secret value is 42!");
